324a733ea9bb070dbe10458503b96877951c0230
mcespo <mcespo@gmail.com>
2022-04-13 22:25:27 -0500

    new logo edits

--- src/components/Brands.svelte
+++ src/components/Brands.svelte
@@ -1,25 +1,26 @@
 <script>
-	import Desh from './../images/DESH 2.png';
-	import Dpr from './../images/DPR 2.png';
-	import Elym from './../images/ELYM 2.png';
-	import Imuntera from './../images/IMUNTERA 2.png';
-	import Iris from './../images/IRIS 2.png';
-	import OmniElectronica from './../images/Omni 2.png';
-	import SchoolGuardian from './../images/School Guardian 2.png';
-	import WeCare from './../images/WeCare 2.png';
+	import Desh from './../images/DESH_edit.png';
+	import Dpr from './../images/DPR_edit.png';
+	import Elym from './../images/ELYM_edit.png';
+	import Imuntera from './../images/IMUNTERA_edit.png';
+	import Iris from './../images/iris_pay_edit.png';
+	import OmniElectronica from './../images/Omni_edit.png';
+	import SchoolGuardian from './../images/School Guardian_edit.png';
+	import WeCare from './../images/WeCare_edit.png';
 
-	import Logo1871 from './../images/1871.png';
-	import Logo2112 from './../images/2112.png';
-	import AmpedKitchen from './../images/amped_kitchens.png';
-	import Brasil from './../images/brasil_consulate.jpeg';
-	import Ccc from './../images/ccc.jpg';
-	import Connectory from './../images/connectory.png';
-	import Dpi from './../images/dpi.png';
-	import Matter from './../images/matter.jpg';
-	import Mhub from './../images/mhub.png';
-	import Pilot from './../images/pilot.jpg';
-	import Polsky from './../images/polsky.png';
-	import Wbc from './../images/wbc.png.crdownload.png';
+	import Logo1871 from './../images/1871_edit.png';
+	import Logo2112 from './../images/2112_edit.png';
+	import AmpedKitchen from './../images/amped_kitchens_edit.png';
+	import Brasil from './../images/brasil_consulate_edit.png';
+	import Ccc from './../images/ccc_edit.png';
+	import Connectory from './../images/connectory_edit.png';
+	import Dpi from './../images/dpi_edit.png';
+	import Matter from './../images/matter_edit.png';
+	import Mhub from './../images/mhub_edit.png';
+	import P33 from './../images/P33_edit.jpg';
+	import Pilot from './../images/pilot_edit.jpg';
+	import Polsky from './../images/polsky_edit.png';
+	import Wbc from './../images/wbc_edit.png';
 </script>
 
 <section id="portfolio" class="bg-white">
@@ -27,59 +28,59 @@
 		<p class="text-center font-semibold uppercase text-gray-600 tracking-wider text-4xl">
 			PORTFOLIO | COHORT I - Brazil > US
 		</p>
-		<div class="mt-6 grid grid-cols-1 gap-0.5 md:grid-cols-4 lg:mt-8">
+		<div class="mt-6 grid grid-cols-2 gap-0.5 md:grid-cols-4 lg:mt-8">
 			<div class="col-span-1 flex justify-center py-8 px-6 bg-gray-50 items-center">
 				<img
-					class="w-48 h-fit"
+					class="w-32 h-auto"
 					src="{Desh}"
 					alt="DASH"
 				/>
 			</div>
 			<div class="col-span-1 flex justify-center py-8 px-6 bg-gray-50 items-center">
 				<img
-					class="w-48 h-fit"
+					class="w-32 h-auto"
 					src="{Dpr}"
 					alt="DPR"
 				/>
 			</div>
 			<div class="col-span-1 flex justify-center py-8 px-6 bg-gray-50 items-center">
 				<img
-					class="w-48 h-fit"
+					class="w-32 h-auto"
 					src="{Elym}"
 					alt="ELYM"
 				/>
 			</div>
 			<div class="col-span-1 flex justify-center py-8 px-6 bg-gray-50 items-center">
 				<img
-					class="w-48 h-fit"
+					class="w-32 h-auto"
 					src="{Imuntera}"
 					alt="Imuntera"
 				/>
 			</div>
 			<div class="col-span-1 flex justify-center py-8 px-6 bg-gray-50 items-center">
 				<img
-					class="w-48 h-fit"
+					class="w-32 h-auto"
 					src="{Iris}"
 					alt="IRIS"
 				/>
 			</div>
 			<div class="col-span-1 flex justify-center py-8 px-6 bg-gray-50 items-center">
 				<img
-					class="w-48 h-fit"
+					class="w-32 h-auto"
 					src="{OmniElectronica}"
 					alt="Omni Electronica"
 				/>
 			</div>
 			<div class="col-span-1 flex justify-center py-8 px-6 bg-gray-50 items-center">
 				<img
-					class="w-48 h-fit"
+					class="w-32 h-auto"
 					src="{SchoolGuardian}"
 					alt="School Guardian"
 				/>
 			</div>
 			<div class="col-span-1 flex justify-center py-8 px-6 bg-gray-50 items-center">
 				<img
-					class="w-48 h-fit"
+					class="w-32 h-auto"
 					src="{WeCare}"
 					alt="School Guardian"
 				/>
@@ -92,87 +93,94 @@
 		<p class="text-center font-semibold uppercase text-gray-600 tracking-wider text-4xl">
 			CHICAGO and ILLINOIS ECOSYSTEM SUPPORT SAMPLE
 		</p>
-		<div class="mt-6 grid grid-cols-1 gap-0.5 md:grid-cols-4 lg:mt-8">
-			<div class="col-span-1 flex justify-center py-8 px-6 bg-gray-50 items-center">
+		<div class="mt-6 grid grid-cols-1 gap-0.5 md:grid-cols-5 lg:mt-8">
+			<div class="col-span-2 flex justify-center py-8 px-6 bg-gray-50 items-center">
 				<img
-					class="w-48 h-fit"
+					class="w-32 h-auto"
 					src="{Logo1871}"
 					alt="School Guardian"
 				/>
 			</div>
 			<div class="col-span-1 flex justify-center py-8 px-6 bg-gray-50 items-center">
 				<img
-					class="w-48 h-fit"
+					class="w-32 h-auto"
 					src="{Logo2112}"
 					alt="School Guardian"
 				/>
 			</div>
 			<div class="col-span-1 flex justify-center py-8 px-6 bg-gray-50 items-center">
 				<img
-					class="w-48 h-fit"
+					class="w-32 h-auto"
 					src="{AmpedKitchen}"
 					alt="School Guardian"
 				/>
 			</div>
 			<div class="col-span-1 flex justify-center py-8 px-6 bg-gray-50 items-center">
 				<img
-					class="w-48 h-fit"
+					class="w-32 h-auto"
 					src="{Brasil}"
 					alt="School Guardian"
 				/>
 			</div>
 			<div class="col-span-1 flex justify-center py-8 px-6 bg-gray-50 items-center">
 				<img
-					class="w-48 h-fit"
+					class="w-32 h-auto"
 					src="{Ccc}"
 					alt="School Guardian"
 				/>
 			</div>
 			<div class="col-span-1 flex justify-center py-8 px-6 bg-gray-50 items-center">
 				<img
-					class="w-48 h-fit"
+					class="w-32 h-auto"
 					src="{Connectory}"
 					alt="School Guardian"
 				/>
 			</div>
 			<div class="col-span-1 flex justify-center py-8 px-6 bg-gray-50 items-center">
 				<img
-					class="w-48 h-fit"
+					class="w-32 h-auto"
 					src="{Dpi}"
 					alt="School Guardian"
 				/>
 			</div>
 			<div class="col-span-1 flex justify-center py-8 px-6 bg-gray-50 items-center">
 				<img
-					class="w-48 h-fit"
+					class="w-32 h-auto"
 					src="{Matter}"
 					alt="School Guardian"
 				/>
 			</div>
 			<div class="col-span-1 flex justify-center py-8 px-6 bg-gray-50 items-center">
 				<img
-					class="w-48 h-fit"
+					class="w-32 h-auto"
 					src="{Mhub}"
 					alt="School Guardian"
 				/>
 			</div>
 			<div class="col-span-1 flex justify-center py-8 px-6 bg-gray-50 items-center">
 				<img
-					class="w-48 h-fit"
+					class="w-32 h-auto"
+					src="{P33}"
+					alt="School Guardian"
+				/>
+			</div>
+			<div class="col-span-1 flex justify-center py-8 px-6 bg-gray-50 items-center">
+				<img
+					class="w-32 h-auto"
 					src="{Pilot}"
 					alt="School Guardian"
 				/>
 			</div>
 			<div class="col-span-1 flex justify-center py-8 px-6 bg-gray-50 items-center">
 				<img
-					class="w-48 h-fit"
+					class="w-32 h-auto"
 					src="{Polsky}"
 					alt="School Guardian"
 				/>
 			</div>
 			<div class="col-span-1 flex justify-center py-8 px-6 bg-gray-50 items-center">
 				<img
-					class="w-48 h-fit"
+					class="w-32 h-auto"
 					src="{Wbc}"
 					alt="School Guardian"
 				/>
