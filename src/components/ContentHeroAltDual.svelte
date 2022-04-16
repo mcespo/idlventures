ab5534e3d8a93dce9fe9ddc16932d660dbaf60b8
mcespo <mcespo@gmail.com>
2022-04-16 01:32:37 -0500

    Jacqueline

--- src/components/ContentHeroAltDual.svelte
+++ src/components/ContentHeroAltDual.svelte
@@ -16,6 +16,16 @@
 					<b>Flavia Andrade</b><br>
 					CEO, IDL Ventures<br>
 					GP, IDL-Ignition
+					<a href="https://www.linkedin.com/in/flavia-andrade/" rel="external" class="text-idl-blue hover:text-idl-gray">
+					<span class="sr-only">LinkedIn</span>
+					<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
+						<path
+							fill-rule="evenodd"
+							d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+				</a>
 				</p>
 			</aside>
 		</div>
@@ -29,11 +39,20 @@ venture growth development.			</p>
 			<aside class="flex gap-4">
 				<img class="mx-auto h-32 w-32 rounded-full" src="./JacquelineKapur.jpeg" alt="Jacqueline Kapur" />
 				<p class="self-center">
-					<b>Jackie Kapur</b><br>
+					<b>Jacqueline Kapur</b><br>
 					Managing Partner, Ignition Capital <br>GP, IDL-Ignition<br>
 					Former CFO, Bloomberg
-				</p>
-			</aside>
+					<a href="https://www.linkedin.com/in/jacqueline-kapur-49b1b81" rel="external" class="text-idl-blue hover:text-idl-gray">
+					<span class="sr-only">LinkedIn</span>
+					<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
+						<path
+							fill-rule="evenodd"
+							d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+				</a>
+				</p>>
 		</div>
 	</div>
 </section>
